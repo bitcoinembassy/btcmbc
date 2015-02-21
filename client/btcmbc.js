b966f230360180940812b1cd23799126ca4a8d01
Francis Brunelle <frabrunelle@gmail.com>
2015-02-21 15:48:02 -0500

    initial commit

---
+++ client/btcmbc.js
@@ -0,0 +1,35 @@
+var bitpay = function(resp) {
+  rates = JSON.parse(resp.content);
+  cad = rates[5]['rate'];
+  usd = rates[0]['rate'];
+  bitpay_rate = cad / usd;
+  HTTP.get("https://openexchangerates.org/api/latest.json?app_id=a5512fe9c98849d18bc5bbcc7074f408", function(err, resp) {
+    real_rate = JSON.parse(resp.content)['rates']['CAD'];
+    difference = bitpay_rate - real_rate;
+    console.log(cad + " CAD | " + usd + " USD | " + bitpay_rate + " | " + real_rate + " | " + difference);
+  });
+  btcmbc = cad * 1.05;
+  Session.set('rate', btcmbc.toFixed(2));
+}
+
+Meteor.startup(function() {
+  HTTP.get("https://bitpay.com/api/rates", function(err, resp) {
+    bitpay(resp);
+  });
+});
+
+Meteor.setInterval(function() {
+  HTTP.get("https://bitpay.com/api/rates", function(err, resp) {
+    bitpay(resp);
+  });
+}, 60000);
+
+
+Template.hello.helpers({
+  rate: function () {
+    return Session.get("rate");
+  }
+});
+Template.hello.rendered = function() {
+  $('h1').fitText();
+};
